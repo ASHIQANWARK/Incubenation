db8161a69fe851d29b1de4f2cc6356842fbc6945
ASHIQANWARK <ashiqanwar777@gmail.com>
2025-07-30 15:58:29 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
+
 // Layout Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -48,7 +48,7 @@ function App() {
           <Route path="/termsandconditions" element={<TermsAndConditions />} />
           <Route path="/refundpolicy" element={<RefundPolicy />} />
         </Route>
-          <Analytics />
+         
         {/* Catch-All Route */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
